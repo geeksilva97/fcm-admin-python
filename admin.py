b345e5296e023053dc13751ba724c04ef63ee9d5
Edigleysson Silva <edigleyssonsilva@gmail.com>
2020-07-19 02:18:33 -0300

    some increments

--- admin.py
+++ admin.py
@@ -1,14 +1,28 @@
 import firebase_admin
 from firebase_admin import credentials, messaging
 import datetime
+import json
 
-cred = credentials.Certificate('config/gooexapp-firebase-adminsdk-xkhb9-2f4864d8c7.json')
+cred = credentials.Certificate('config/firebase-admin.json')
 firebase_admin.initialize_app(cred)
 
 def send_notification(title, message, to=[], data={}):
     # registration_tokens = [
     #     'dgtKyE3PzDgtJz_NxaSP5A:APA91bGYm8jkmLt-k3ZWquUsQy_zwOPigaK_iO-TYgYNJHO29L3P42jFGBb7XWLXApXErtNU18-yZucN37fA2AG35lB4d23YsI3pXns6l7s5BzEadWcqsM_qZdYFHtPmAZwXO2ZcHomE'
     # ]
+    # data['click_action'] = 'FLUTTER_NOTIFICATION_CLICK'
+    # data['title'] = 'MY TITLE'
+    # data['body'] = 'MY BODY'
+    data.update({
+        "body": message,
+        "title": title,
+        'type': 'confimar_embarque', # aqui vai a informacao sobre o tipo da notificacao
+        'email': 'eutransportador@gmail.com', # aqui vai o email do usuário
+        "click_action": "FLUTTER_NOTIFICATION_CLICK",
+        "id": "1",
+        "status": "done",
+        'data': json.dumps({'email': 'eutransportador@gmail.com'})
+    })
     registration_tokens = to
 
     message = messaging.MulticastMessage(
@@ -17,13 +31,16 @@ def send_notification(title, message, to=[], data={}):
             body=message,
         ),
         android=messaging.AndroidConfig(
-            ttl=datetime.timedelta(seconds=3600),
-            priority='normal',
+            ttl=datetime.timedelta(seconds=5*60),
+            priority='high',
             
             notification=messaging.AndroidNotification(
+            #     title=title,
+            # b   body=message,
                 icon='stock_ticker_update',
                 color='#f45342',
                 sound='default',
+                # click_action='FLUTTER_NOTIFICATION_CLICK',
             ),
         ),
         data=data,
@@ -32,6 +49,7 @@ def send_notification(title, message, to=[], data={}):
     response = messaging.send_multicast(message)
     # See the BatchResponse reference documentation
     # for the contents of response.
+    print('data: {}\n'.format(data))
     print('{0} messages were sent successfully'.format(response.success_count))
 
 
--- test_fcm.py
+++ test_fcm.py
@@ -1,16 +1,16 @@
 from admin import send_notification
 
-token_web = 'dgtKyE3PzDgtJz_NxaSP5A:APA91bGYm8jkmLt-k3ZWquUsQy_zwOPigaK_iO-TYgYNJHO29L3P42jFGBb7XWLXApXErtNU18-yZucN37fA2AG35lB4d23YsI3pXns6l7s5BzEadWcqsM_qZdYFHtPmAZwXO2ZcHomE'
-token_mobile = 'enZBYzC-MJg:APA91bEQnFjawk4A4trokV5MUTmDbs6uKZKE2lfNVgs668Vw0ySzbG2xjI-3qLOob_IVAeos3tgZQ0T2AxYnlPnrqD-t_4CKLlEH-CgUctlMh8xjcOxMMwjKgX9JYmxTPr1bcptvEG_L'
+# token_web = 'dgtKyE3PzDgtJz_NxaSP5A:APA91bGYm8jkmLt-k3ZWquUsQy_zwOPigaK_iO-TYgYNJHO29L3P42jFGBb7XWLXApXErtNU18-yZucN37fA2AG35lB4d23YsI3pXns6l7s5BzEadWcqsM_qZdYFHtPmAZwXO2ZcHomE'
+token_mobile = 'cK09jkhyF1I:APA91bH9jNwcvzH_TZDTveVkO4fiJwzbTSNZHNtQYacGYhTUJ5fcB9SRBLtikio5C4CVgRYOaN8lOOlEUjqRy8R5y3ZM9EneiAXbVw3pNwWIeAgfBkZpaOcE4slOUvE3WaDKShN-bw8e'
 
-print('Informe o título:')
-title = input()
+# print('Informe o título:')
+# title = input()
 
-print('Informe o corpo:')
-message = input()
+# print('Informe o corpo:')
+# message = input()
 
 send_notification(
-    title,
-    message,
-    to=[token_mobile, token_web]
+    'Gooex',
+    'Cansado dessa porra!!',
+    to=[token_mobile]
 )
